1f17993b3f961ed5d11ab52b43b8fd56901b9798
SindreSau <sindre_50@hotmail.com>
2025-05-06 23:18:28 +0200

    feat: updated hero-section with nice icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,27 +3,29 @@ import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
+    vite: {
+        plugins: [tailwindcss()],
+    },
 
-  i18n: {
-    locales: ['en', 'nb'],
-    defaultLocale: 'en',
-  },
+    i18n: {
+        locales: ['en', 'nb'],
+        defaultLocale: 'en',
+    },
 
-  prefetch: {
-    prefetchAll: true,
-    defaultStrategy: 'viewport',
-  },
+    prefetch: {
+        prefetchAll: true,
+        defaultStrategy: 'viewport',
+    },
 
-  experimental: {
-    clientPrerender: true,
-  },
+    experimental: {
+        clientPrerender: true,
+    },
 
-  integrations: [sitemap()],
+    integrations: [sitemap(), icon()],
 
-  site: 'https://www.sindresau.me',
+    site: 'https://www.sindresau.me',
 });
